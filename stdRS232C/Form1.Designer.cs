20657df3d7526d4dd2c4d7f5293724eb2ea1a3b0
Jominkyu1 <jominkyu@gmail.com>
2024-07-15 16:18:31 +0900

    RS232C Serial Study

---
+++ stdRS232C/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace stdRS232C
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbSerialPort = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtSendReceive = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbBaudRate = new System.Windows.Forms.ComboBox();
+            this.cbDatabits = new System.Windows.Forms.ComboBox();
+            this.cbParity = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cbSerialPort
+            // 
+            this.cbSerialPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSerialPort.FormattingEnabled = true;
+            this.cbSerialPort.Location = new System.Drawing.Point(123, 9);
+            this.cbSerialPort.Name = "cbSerialPort";
+            this.cbSerialPort.Size = new System.Drawing.Size(135, 20);
+            this.cbSerialPort.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(337, 3);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(110, 30);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "CONNECT";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtSendReceive
+            // 
+            this.txtSendReceive.Location = new System.Drawing.Point(12, 154);
+            this.txtSendReceive.Name = "txtSendReceive";
+            this.txtSendReceive.Size = new System.Drawing.Size(435, 253);
+            this.txtSendReceive.TabIndex = 2;
+            this.txtSendReceive.Text = "";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(337, 413);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(110, 21);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "SEND";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Serial Port";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "BaudRate";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Data Size (bits)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Parity";
+            // 
+            // cbBaudRate
+            // 
+            this.cbBaudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbBaudRate.FormattingEnabled = true;
+            this.cbBaudRate.Location = new System.Drawing.Point(123, 50);
+            this.cbBaudRate.Name = "cbBaudRate";
+            this.cbBaudRate.Size = new System.Drawing.Size(135, 20);
+            this.cbBaudRate.TabIndex = 11;
+            // 
+            // cbDatabits
+            // 
+            this.cbDatabits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDatabits.FormattingEnabled = true;
+            this.cbDatabits.Location = new System.Drawing.Point(123, 80);
+            this.cbDatabits.Name = "cbDatabits";
+            this.cbDatabits.Size = new System.Drawing.Size(135, 20);
+            this.cbDatabits.TabIndex = 12;
+            // 
+            // cbParity
+            // 
+            this.cbParity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbParity.FormattingEnabled = true;
+            this.cbParity.Location = new System.Drawing.Point(123, 111);
+            this.cbParity.Name = "cbParity";
+            this.cbParity.Size = new System.Drawing.Size(135, 20);
+            this.cbParity.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(311, 50);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 12);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Status";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblStatus.Location = new System.Drawing.Point(311, 80);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(11, 12);
+            this.lblStatus.TabIndex = 16;
+            this.lblStatus.Text = "-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(311, 111);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 12);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Your PID";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblID.Location = new System.Drawing.Point(373, 111);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(15, 13);
+            this.lblID.TabIndex = 19;
+            this.lblID.Text = "-";
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(12, 413);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(293, 21);
+            this.txtSend.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 447);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cbParity);
+            this.Controls.Add(this.cbDatabits);
+            this.Controls.Add(this.cbBaudRate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSendReceive);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cbSerialPort);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RS 232C";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbSerialPort;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.RichTextBox txtSendReceive;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbBaudRate;
+        private System.Windows.Forms.ComboBox cbDatabits;
+        private System.Windows.Forms.ComboBox cbParity;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtSend;
+    }
+}
+
---
+++ stdRS232C/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stdRS232C
+{
+    public partial class Form1 : Form
+    {
+        private readonly string PID = Process.GetCurrentProcess().Id.ToString();
+        private readonly SerialPort sPort = new SerialPort();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sPort.DataReceived += OnDataReceived;
+
+            lblID.Text = PID;
+
+            InitSerialItems();
+        }
+
+        private void InitSerialItems()
+        {
+            //Serial Ports
+            foreach (string port in SerialPort.GetPortNames()) cbSerialPort.Items.Add(port);
+            cbSerialPort.SelectedIndex = cbSerialPort.Items.Count - 1;
+
+            //Baud Rate
+            int rate = 600;
+            for (int i = 0; i < 5; i++)
+            {
+                cbBaudRate.Items.Add(rate);
+                rate *= 2;
+            }
+            cbBaudRate.SelectedIndex = cbBaudRate.Items.Count - 1;
+            
+            //DataSize
+            cbDatabits.Items.Add(8);
+            cbDatabits.SelectedIndex = 0;
+
+            //Parity
+            cbParity.Items.Add("NONE");
+            cbParity.SelectedIndex = 0;
+            cbParity.Enabled = false;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            //이미 연결되어있다면
+            if (sPort.IsOpen)
+            {
+                sPort.Close();
+                DisplayEnableDisable();
+            }
+            else
+            {
+                try
+                {
+                    sPort.PortName = cbSerialPort.SelectedItem.ToString();
+                    sPort.BaudRate = (int)cbBaudRate.SelectedItem;
+                    sPort.DataBits = (int)cbDatabits.SelectedItem;
+                    sPort.Parity = Parity.None;
+                    sPort.Encoding = Encoding.UTF8;
+                    
+                    sPort.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+
+                DisplayEnableDisable();
+            }
+        }
+
+        private void OnDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string receivedString = sPort.ReadExisting();
+            this.Invoke(new MethodInvoker(() =>
+            {
+                txtSendReceive.AppendText(receivedString);
+            }));
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string msg = $"[{PID}] {txtSend.Text}\r\n";
+
+                sPort.Write(msg);
+                txtSendReceive.AppendText(msg);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            txtSend.Text = "";
+            txtSend.Focus();
+        }
+
+        private void DisplayEnableDisable()
+        {
+            cbSerialPort.Enabled = !sPort.IsOpen;
+            cbBaudRate.Enabled = !sPort.IsOpen;
+            cbDatabits.Enabled = !sPort.IsOpen;
+
+            lblStatus.Text = sPort.IsOpen ? "CONNECTED" : "NOT CONNECTED";
+            btnConnect.Text = sPort.IsOpen ? "DISCONNECT" : "CONNECT";
+        }
+
+    }
+}
